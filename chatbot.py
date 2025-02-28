bd47e68da2613e1f88d3cba9289eb925228ece7e
Tahsin Rahman <tr174@duke.edu>
2025-02-27 23:58:09 -0500

    initial commit

---
+++ chatbot.py
@@ -0,0 +1,9 @@
+# pip install streamlit
+# pip install langchain
+# pip install -qU langchain-ollama
+
+# 'streamlit run chatbot.py' to run
+import streamlit as st
+st.title("Chat App with Ollama and Langchain (with Chat History)")
+
+
